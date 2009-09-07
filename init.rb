ff6f5da633b160bfbcf6429b239eecfe4cca55e5
Christopher Mann <christopher@mann.fr>
2009-09-07 23:52:26 +0200

    My first commit of redmine_workload after generate redmine_plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_workload do
+  name 'Redmine Workload plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
