6372f718c0a851a15e6a1f298ba2a0b242459cd9
Christopher Mann <christopher@mann.fr>
2009-09-12 11:01:23 +0200

    Migration underway

--- init.rb
+++ init.rb
@@ -1,8 +1,18 @@
 require 'redmine'
 
+# Patches to the Redmine core.
+require 'dispatcher'
+require 'issue_patch'
+require 'user_patch'
+Dispatcher.to_prepare do
+  Issue.send(:include, IssuePatch)
+  Query.send(:include, UserPatch)
+end
+
+
 Redmine::Plugin.register :redmine_workload do
   name 'Redmine Workload plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Christopher Mann <christopher@mann.fr>'
+  description 'This plugin handles workload estimations for Redmine.'
   version '0.0.1'
 end
